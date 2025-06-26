85c771a6998cd7a58942aca5c1ab9ebc4cf173f4
RevampedCity <jpruitt612@yahoo.com>
2025-06-25 22:18:59 -0500

    Update animation.py

--- utils/animation.py
+++ utils/animation.py
@@ -1,1 +1,26 @@
-animation.py
+import os
+import time
+import platform
+
+def clear():
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+
+def wait(seconds=2):
+    time.sleep(seconds)
+
+def skull_bump_animation():
+    frames = [
+        "    (-_-)",
+        "     (-_-) .",
+        "      (-_-) ..",
+        "       (-_-) ...",
+        "        (-_-)....",
+        "         (-_-).....",
+        "          (o_o) *sniff*",
+        "          (o_o)!!"
+    ]
+    for frame in frames:
+        clear()
+        print("\n\n\n\n\n")
+        print(frame.center(80))
+        time.sleep(0.2)
